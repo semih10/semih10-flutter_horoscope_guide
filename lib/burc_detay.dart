4d9bb4b60da09c3fee37bb6520cbb762c5c446fd
Semih BARUT <semibarut@gmail.com>
2020-05-14 19:01:02 +0300

    third commit -Burc detail page is updated

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutterburclar/burc_listesi.dart';
+import 'package:flutterburclar/models/burc.dart';
 
 class BurcDetay extends StatelessWidget {
+  int indeks;
+  Burc secilenBurc;
+
+  BurcDetay(this.indeks);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    secilenBurc = BurcListesi.tumBurclar[indeks];
+    return Scaffold(
+      body: CustomScrollView(
+        primary: false,
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 250,
+            pinned: true,
+            primary: true,
+            backgroundColor: Colors.pink,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(secilenBurc.burcAdi + " Burcu ve Özellikleri"),
+              background: Image.asset(
+                "images/" + secilenBurc.burcBuyukResim,
+                fit: BoxFit.cover,
+              ),
+              centerTitle: true,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              child: Text(
+                secilenBurc.burcDetayi,
+                style: TextStyle(color: Colors.black, fontSize: 18),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/burc_listesi.dart
+++ lib/burc_listesi.dart
@@ -1,35 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutterburclar/burc_detay.dart';
 import 'package:flutterburclar/models/burc.dart';
 import 'package:flutterburclar/utils/strings.dart';
 
-
 class BurcListesi extends StatelessWidget {
-  
+  static List<Burc> tumBurclar;
+
   @override
   Widget build(BuildContext context) {
-    List<Burc> tumBurclar=veriKaynaginiHazirla();
-    
+    tumBurclar = veriKaynaginiHazirla();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Burç Rehberi"),
       ),
-      body: Container(),
+      body: listeyiHazirla(),
     );
   }
 
   List<Burc> veriKaynaginiHazirla() {
-    List<Burc> burclar=[];
+    List<Burc> burclar = [];
 
-    for(int i=0;i<12;i++){
-      String kucukResim=Strings.BURC_ADLARI[i].toLowerCase()+"${i+1}"+".png";
-      String buyukResim=Strings.BURC_ADLARI[i].toLowerCase()+"_buyuk"+"${i+1}"+".png";
+    for (int i = 0; i < 12; i++) {
+      String kucukResim =
+          Strings.BURC_ADLARI[i].toLowerCase() + "${i + 1}" + ".png";
+      String buyukResim =
+          Strings.BURC_ADLARI[i].toLowerCase() + "_buyuk" + "${i + 1}" + ".png";
 
-      Burc eklenecekBurc=Burc(Strings.BURC_ADLARI[i],Strings.BURC_TARIHLERI[i],Strings.BURC_GENEL_OZELLIKLERI[i]
-          ,kucukResim,buyukResim);
+      Burc eklenecekBurc = Burc(
+          Strings.BURC_ADLARI[i],
+          Strings.BURC_TARIHLERI[i],
+          Strings.BURC_GENEL_OZELLIKLERI[i],
+          kucukResim,
+          buyukResim);
       burclar.add(eklenecekBurc);
     }
     return burclar;
   }
 
-  
+  Widget listeyiHazirla() {
+    return ListView.builder(
+      itemCount: tumBurclar.length,
+      itemBuilder: (BuildContext context, int index) {
+        return tekSatirBurc(context, index);
+      },
+    );
+  }
+
+  Widget tekSatirBurc(BuildContext context, int index) {
+    Burc oAnkiBurc = tumBurclar[index];
+    return Card(
+      elevation: 4,
+      child: ListTile(
+        onTap:()=> Navigator.push(context, MaterialPageRoute(builder: (context)=>BurcDetay(index))),
+        leading: Image.asset(
+          "images/" + oAnkiBurc.burcKucukResim,
+          width: 64,
+          height: 64,
+        ),
+        title: Text(
+          oAnkiBurc.burcAdi,
+          style: TextStyle(
+              fontWeight: FontWeight.w400, color: Colors.pink.shade500,fontSize: 24),
+        ),
+        subtitle: Text(
+          oAnkiBurc.burcTarihi,
+          style: TextStyle(
+              fontWeight: FontWeight.w200, color: Colors.black,fontSize: 18),
+        ),
+        trailing: Icon(Icons.arrow_forward_ios,color: Colors.pink,),
+      ),
+    );
+  }
 }
