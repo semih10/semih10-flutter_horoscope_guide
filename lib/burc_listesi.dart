bf24a74acb69a766362af85df08133ef706b3cca
Semih BARUT <semibarut@gmail.com>
2020-05-14 17:03:42 +0300

    second commit -Burc class is created -Horoscope list including all horoscope data is prepared

--- lib/burc_listesi.dart
+++ lib/burc_listesi.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutterburclar/models/burc.dart';
+import 'package:flutterburclar/utils/strings.dart';
+
 
 class BurcListesi extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<Burc> tumBurclar=veriKaynaginiHazirla();
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Burç Rehberi"),
+      ),
+      body: Container(),
+    );
   }
+
+  List<Burc> veriKaynaginiHazirla() {
+    List<Burc> burclar=[];
+
+    for(int i=0;i<12;i++){
+      String kucukResim=Strings.BURC_ADLARI[i].toLowerCase()+"${i+1}"+".png";
+      String buyukResim=Strings.BURC_ADLARI[i].toLowerCase()+"_buyuk"+"${i+1}"+".png";
+
+      Burc eklenecekBurc=Burc(Strings.BURC_ADLARI[i],Strings.BURC_TARIHLERI[i],Strings.BURC_GENEL_OZELLIKLERI[i]
+          ,kucukResim,buyukResim);
+      burclar.add(eklenecekBurc);
+    }
+    return burclar;
+  }
+
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Burç Rehberi",
       theme: ThemeData(
         primarySwatch: Colors.pink
