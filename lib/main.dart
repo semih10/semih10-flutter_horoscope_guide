940b5fe320725a68d0e80611e1444c3a1f784dba
Semih BARUT <semibarut@gmail.com>
2020-05-14 16:11:57 +0300

    initial commit -lists about horoscopes are available -images are available

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutterburclar/burc_listesi.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Burç Rehberi",
+      theme: ThemeData(
+        primarySwatch: Colors.pink
+      ),
+      home: BurcListesi(),
+
+    );
+  }
+
+}
